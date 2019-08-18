58b3d1163b624651ec8a2b8f5c7ffe588a058442
dianamdc <53751457+dianamdc@users.noreply.github.com>
2019-08-18 10:28:19 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -27,7 +27,7 @@ public class Main {
         graph.addEdge("bus", 3, 4, 10, 5, 30);
         graph.addEdge("bus", 4, 5, 10, 5, 30);
 
-        graph.findShortestDistance("distance");
+        graph.findShortestDistance();
         //System.out.println(graph.getTerminals());
     }
 
